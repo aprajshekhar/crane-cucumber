d8f4571c841176532b3f708b81f86a48155d64a4
A.P. Rajshekhar <randalap@redhat.com>
2015-09-29 15:34:08 +0530

    CP-275: Automated testing - status, search and repositories test. debug

--- crane-cucumber/test/features/stepdef_helpers/configuration.rb
+++ crane-cucumber/test/features/stepdef_helpers/configuration.rb
@@ -7,8 +7,10 @@ class Configuration
 
   def self.load name
     @@config = nil
-    p "config path: " + ENV['CONFIG_FILE'] + "/support/config.yaml"
-    io = ENV['CONFIG_FILE'] || File.open( File.dirname(__dir__) + "/support/config.yaml" )
+    path = ENV['CONFIG_FILE'] ||  File.dirname(__dir__)
+    p "config path: "
+    p ENV['CONFIG_FILE']
+    io =  File.open(path + "/support/config.yaml" )
     YAML::load_documents(io) { |doc| @@config = doc[name] }
     raise "Could not locate a configuration named \"#{name}\"" unless @@config
   end
@@ -20,4 +22,4 @@ class Configuration
 end
 
 raise "Please set the TEST_ENV environment variable" unless ENV['TEST_ENV']
-raise "Please set the TEST_ENV environment variable" unless ENV['DIR']
+raise "Please set the DIR environment variable" unless ENV['DIR']
